93d3c5a89aab1ddfe02692f175f001a58267f997
vitoredu2230 <vitoredu.wi@gmail.com>
2026-03-23 15:27:27 -0300

    Working on Cards

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const userName = "Cubo";
+
+  const dataList = [
+    { name: "Charmander", price: 19.0, category: "Fire", onSale: true },
+    { name: "Pikachu", price: 50.0, category: "Thunder", onSale: true },
+    { name: "Dragonite", price: 150.0, category: "Dragon", onSale: false },
+    { name: "Gyarados", price: 100.0, category: "Water", onSale: false },
+    { name: "MewTwo", price: 1000000.0, category: "Psychic", onSale: true },
+    { name: "Ratatta", price: 0.5, category: "Mouse", onSale: true },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Welcome, {userName}!</Text>
+      <Text style={styles.subtitle}>
+        There are the list of Pokémons that we're selling!
+      </Text>
+
+      {dataList.map((pokemon, index) => (
+        <View key={index}>
+          <Text style={styles.pokemonCard}>{pokemon.name}</Text>
+          <Text>{pokemon.category}</Text>
+          <Text>{pokemon.price}</Text>
+          <Text>{pokemon.onSale && <Text>Available</Text>}</Text>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  pokemonCard: {
+    padding: 15,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    marginBottom: 10,
+    borderLeftWidth: 4,
+  },
+  title: {
+    fontSize: 25,
+  },
+  subtitle: {
+    fontSize: 17,
+  },
+  pokemonName: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  fire: {
+    color: "orange",
   },
 });
